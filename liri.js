b472e02084ec582381609a5d6c80681695c89d6f
Kelvin Almonte <kelvin.almonte95@gmail.com>
2019-11-09 19:01:27 -0500

    Progress in the code. imdb API working. Spotify needs fixing.

--- liri.js
+++ liri.js
@@ -1,11 +1,72 @@
+// set any environment variables with the .env package
 var dotenv = require("dotenv").config();
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
 
-console.log(spotify);
+// Here we will require data from fs file package
+var fs = require("fs");
+
+var keys = require("./keys.js");
+var axios = require("axios");
+
+// console.log(spotify);
+// var spotify = new Spotify(keys.spotify);
+
+var action = process.argv[2];
+var userInput = process.argv[3];
+
+// We will then create a switch-case statement (if-else would also work).
+// The switch-case will direct which function gets run.
+switch (action) {
+    case "movie-this":
+        movie();
+        break;
+
+    case "spotify-this-song":
+        spotify();
+        break;
+
+    case "concert-this":
+        bands();
+        break;
+
+    case "do-what-it-says":
+        whatItSays();
+        break;
+}
 
 var artist = "";
 
-var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+var bandsURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
-// https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp
+
+// axios.get(bandsURL).then(function(response) {
+//     console.log("The artist you're searching for is.. " + response);
+
+// });
+
+function movie() {
+    var omdbURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    var movieName = "";
+
+    axios.get(omdbURL).then(function(response) {
+        // console.log(response.data);
+
+        console.log("-------------------------")
+            // Title of the movie
+        console.log(response.data.Title);
+        // Year the movie came out.
+        console.log(response.data.Year);
+        // IMDB Rating of the movie.
+        console.log(response.data.imdbRating);
+        // Rotten Tomatoes Rating of the movie.
+        // console.log(response.data)
+        // Country where the movie was produced.
+        console.log(response.data.Country);
+        // Language of the movie.
+        console.log(response.data.Language);
+        // Plot of the movie.
+        console.log(response.data.Plot);
+        // Actors in the movie.
+        console.log(response.data.Actors);
+        console.log("-------------------------")
+    });
+}
