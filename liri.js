13d8c7d0f28dcae2c70cb2325b1254e32a322624
Kelvin Almonte <kelvin.almonte95@gmail.com>
2019-11-12 13:44:29 -0500

    I am now able to receive data from Spotify API.

--- liri.js
+++ liri.js
@@ -2,7 +2,7 @@
 var dotenv = require("dotenv").config();
 var keys = require("./keys.js");
 var axios = require("axios");
-var spotify = require('node-spotify-api');
+var Spotify = require('node-spotify-api');
 var fs = require("fs");
 var moment = require("moment");
 moment().format();
@@ -10,7 +10,7 @@ moment().format();
 var userInput = process.argv[2];
 var action = process.argv.slice(3).join(" ");
 
-switch (action, userInput) {
+switch (userInput) {
     case "movie-this":
         movie(action);
         break;
@@ -20,7 +20,7 @@ switch (action, userInput) {
         break;
 
     case "concert-this":
-        bands(action);
+        bands();
         break;
 
     case "do-what-it-says":
@@ -28,6 +28,7 @@ switch (action, userInput) {
         break;
 }
 
+// omdb Movie function
 function movie(movieName) {
 
     if (!movieName) {
@@ -38,7 +39,7 @@ function movie(movieName) {
 
     axios.get(omdbURL).then(function(response) {
 
-        console.log("-------------------------");
+        console.log("============================");
         // Title of the movie
         console.log("Title: " + response.data.Title);
         // Year the movie came out.
@@ -55,31 +56,63 @@ function movie(movieName) {
         console.log("Plot: " + response.data.Plot);
         // Actors in the movie.
         console.log("Actors: " + response.data.Actors);
-        console.log("-------------------------");
+        console.log("============================");
     });
 }
 
+// Bands in Town function
 function bands(artistName) {
 
-    var artistName = action;
+    // var artistName = action;
     var bandsURL = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
-
 
     axios.get(bandsURL).then(function(response) {
         console.log(response);
 
-        console.log("-------------------------");
+        console.log("============================");
+        // Artist Name
+        console.log("Artist name " + response.data.artist);
         // Name of the venue
         console.log("Name of the venue: " + response.data[0].venue.name);
         // Venue location
         console.log("Venue location: " + response.data[0].venue.city);
         // Date of the Event(use moment to format this as "MM/DD/YYYY")
         console.log("Date of the Event " + moment(response.data.datetime).format("MM-DD-YYYY"));
-        console.log("-------------------------");
+        console.log("============================");
     });
 }
 
-// function spotify() {
-//     // placed the secret ID for spotify
-//     var spotify = new spotify(keys.spotify);
-// }
+// Spotify function
+function spotify(songName) {
+    // placed the secret ID for spotify
+    var spotify = new Spotify(keys.spotify);
+
+    // test
+    // console.log(spotify);
+
+    spotify.search({
+        type: "track",
+        query: songName
+
+    }, function(err, data) {
+
+        if (err) {
+            return console.log(err);
+        }
+
+        if (!err) {
+            var songInfo = data.tracks.items;
+
+            // loop through all song tracks
+            for (var i = 0; i < songInfo.length; i++) {
+                // iterate though all song infos
+                songInfo[i];
+                console.log("============================");
+                console.log("Artist name: " + songInfo[i].artists[0].name);
+                console.log("Song name: " + songInfo[i].name);
+                console.log("Preview song " + songInfo[i].preview_url);
+                console.log("Album " + songInfo[i].album.name);
+            }
+        }
+    });
+}
