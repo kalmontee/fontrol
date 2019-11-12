9d90c1019ae24797fa83abfdfcb7ce3018eb3225
Kelvin Almonte <kelvin.almonte95@gmail.com>
2019-11-12 13:45:25 -0500

    Removed comments

--- keys.js
+++ keys.js
@@ -4,9 +4,3 @@ exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
-
-// Need keys for imdb
-
-// Need keys for spotify
-
-// Need keys for bands in town
